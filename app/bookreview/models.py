f79a39f1341b3eb1cc623590752a68c555395b86
Shreyas <ssk272@cornell.edu>
2015-10-10 16:58:46 -0400

    user model added

--- app/bookreview/models.py
+++ app/bookreview/models.py
@@ -1,5 +1,19 @@
 from django.db import models
 
+class UserManager(models.Manager):
+    pass 
+
+class User(models.Model):
+    objects = UserManager()
+    first_name = models.CharField(max_length=200)
+    last_name = models.CharField(max_length=200)
+    phone_number = models.CharField(max_length=12)
+    bank_account = models.CharField(max_length=15)
+    password = models.CharField(max_length=25)
+    bank_name = models.CharField(max_length=100)
+    def __unicode__(self):
+        return '{} {}'.format(self.first_name, self.last_name, self.phone_number, self.bank_account, self.password, self.bank_name)
+
 
 class AuthorManager(models.Manager):
     pass
--- app/bookreview/urls.py
+++ app/bookreview/urls.py
@@ -7,7 +7,8 @@ from bookreview import views
 urlpatterns = patterns(
     '',
     url(r'^$', views.index_view, name='index_view'),
-    url(r'^authors/$', views.test_author, name='author-list')
+    url(r'^authors/$', views.test_author, name='author-list'),
+    url(r'^login/$', views.authenticate_user, name="authenticate_user")
 )
 
 urlpatterns = format_suffix_patterns(urlpatterns)
