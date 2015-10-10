1c9069825a4a2af3bac1a3464b78d88f3590e8c3
redenter <ashwinnsd@gmail.com>
2015-10-10 17:23:38 -0400

    finished modifying models

--- app/bookreview/models.py
+++ app/bookreview/models.py
@@ -15,17 +15,17 @@ class User(models.Model):
         return '{} {}'.format(self.first_name, self.last_name, self.phone_number, self.bank_account, self.password, self.bank_name)
 
 
-class AuthorManager(models.Manager):
-    pass
+# class AuthorManager(models.Manager):
+#     pass
 
 
-class Author(models.Model):
-    objects = AuthorManager()
-    first_name = models.CharField(max_length=200)
-    last_name = models.CharField(max_length=200)
+# class Author(models.Model):
+#     objects = AuthorManager()
+#     first_name = models.CharField(max_length=200)
+#     last_name = models.CharField(max_length=200)
 
-    def __unicode__(self):
-        return '{} {}'.format(self.first_name, self.last_name)
+#     def __unicode__(self):
+#         return '{} {}'.format(self.first_name, self.last_name)
 
 class ChallengeManager(models.Manager):
     pass
@@ -33,9 +33,14 @@ class ChallengeManager(models.Manager):
 
 class Challenge(models.Manager):
     objects = ChallengeManager()
-    title = models.CharField(max_length=200)
-    isbn = models.CharField(max_length=20)
-    author = models.ForeignKey(Author, related_name='books')
+    challenge_name = models.CharField(max_length=50)
+    challenge_details = models.CharField(max_length=200)
+    start_date = models.DateField()
+    end_date = models.DateField()
+    def __unicode__(self):
+        return '{} {}'.format(self.challenge_name, challenge_details, self.start_date, self.end_date)
+
+
 
 
 class TeamManager(models.Manager):
@@ -44,13 +49,25 @@ class TeamManager(models.Manager):
 class Team(models.Model):
     objects = TeamManager()
     members = ListField()
-
-
-class Book(models.Model):
-    objects = BookManager()
-    title = models.CharField(max_length=200)
-    isbn = models.CharField(max_length=20)
-    author = models.ForeignKey(Author, related_name='books')
+    challenge_name = models.ForeignKey(Challenge,related_name='challenge_name')
+    status = models.DecimalField(max_digits=5,decimal_places = 2)
 
     def __unicode__(self):
-        return self.title
+        return '{} {}'.format(self.members,self.status)
+
+
+class ActivityFeedManager(models.Manager):
+    pass
+
+class ActivityFeed(models.Model):
+    objects = ActivityFeedManager()
+
+
+# class Book(models.Model):
+#     objects = BookManager()
+#     title = models.CharField(max_length=200)
+#     isbn = models.CharField(max_length=20)
+#     author = models.ForeignKey(Author, related_name='books')
+
+    # def __unicode__(self):
+    #     return self.title
